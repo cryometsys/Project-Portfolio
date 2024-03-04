e2f5d9373be8bdd60e83b1e8eafbc51667e514bc
BChy <BChy@example.com>
2024-03-05 02:34:24 +0600

    Database functionality added for form submission

---
+++ php/mail.php
@@ -0,0 +1,24 @@
+<?php
+
+$conn = new mysqli('localhost', 'root', '', 'portfolioDB');
+if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["mail"];
+    $message = $_POST["message"];
+
+    $name = mysqli_real_escape_string($conn, $name);
+    $email = mysqli_real_escape_string($conn, $email);
+    $message = mysqli_real_escape_string($conn, $message);
+
+    $sql = "INSERT INTO mailtable (name, mail, text) VALUES ('$name', '$email', '$message')";
+    if ($conn->query($sql) === TRUE) {
+        echo "<script>window.location='../index.html';</script>";
+        $conn->close();
+        http_response_code(200);
+    } else {
+        http_response_code(500);
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
